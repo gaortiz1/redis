e0946d52973f5bce596f7116b96096750c764865
Gabriel <gabriel.ortiz@ninjarmm.com>
2022-03-07 00:47:04 -0500

    [@gaortiz1] add swager

--- src/main/java/com/witbooking/redis/RedisApplication.java
+++ src/main/java/com/witbooking/redis/RedisApplication.java
@@ -1,11 +1,13 @@
 package com.witbooking.redis;
 
+import com.github.lkqm.spring.api.version.EnableApiVersioning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableApiVersioning
 public class RedisApplication {
 
     public static void main(String[] args) {
